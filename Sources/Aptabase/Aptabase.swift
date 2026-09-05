0a3def9f91b1caa178b00131b1eba1820dde6ef5
Joshua Wolfson <j.a.wolfson@icloud.com>
2026-09-06 09:13:54 +1000

    Implemented Aptabase Error Tracking

--- Sources/Aptabase/Aptabase.swift
+++ Sources/Aptabase/Aptabase.swift
@@ -65,6 +65,10 @@ public class Aptabase: NSObject {
 
         enqueueEvent(eventName, with: codable)
     }
+    
+    public func trackError(_ error: Error, fatal: Bool = false) {
+        enqueueErrorReport(error, fatal: fatal)
+    }
 
     /// Initializes the client with given App Key.
     /// - Parameter appKey: The App Key to use.
@@ -91,6 +95,10 @@ public class Aptabase: NSObject {
 
         enqueueEvent(eventName, with: codable)
     }
+    
+    @objc public func trackError(_ error: NSError, fatal: Bool = false) {
+           enqueueErrorReport(error)
+    }
 
     /// Forces all queued events to be sent to the server
     @objc public func flush() {
@@ -105,6 +113,14 @@ public class Aptabase: NSObject {
         }
 
         client.trackEvent(eventName, with: props)
+    }
+    
+    private func enqueueErrorReport(_ error: Error, fatal: Bool = false) {
+        guard let client else {
+            return
+        }
+        
+        client.trackError(error, fatal: fatal)
     }
 
     @objc private func startPolling() {
